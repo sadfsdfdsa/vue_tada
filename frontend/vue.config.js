96d5bcc6db2b6ceb2df53cc2a4618d822736c0c4
sadfsdfdsa <shuvaev13@mail.ru>
2020-07-24 20:09:13 +0500

    init2

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,12 +4,4 @@ module.exports = {
     assetsDir: '',
     outputDir: path.resolve(__dirname, "../static"),
     productionSourceMap: false,
-    devServer: {
-        proxy: {
-            '^/api/*': {
-                // proxies request for api to localhost:5000
-                target: "http://localhost:5000/"
-            },
-        }
-    },
 };
